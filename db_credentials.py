59b4b8147a7194288eefe21a59e2d4b06be7d1bc
David Chunho Lee <davidchunholee@gmail.com>
2020-11-15 21:11:45 -0500

    Created connection with database for admin
    Connection made to database;
    Pre-populated data on admin page using database MySQL queries

---
+++ db_credentials.py
@@ -0,0 +1,9 @@
+host = 'p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+user = 'kjb1ne5vm52w1mio'
+pw = ''
+db = 'vmodqozl2l4ktlbm'
+
+# host = 'p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+# user = 'kjb1ne5vm52w1mio'
+# pw = 'flx8ltu5b8lnfarz'
+# db = 'vmodqozl2l4ktlbm'
