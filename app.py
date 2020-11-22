7a6daa4fe7819d1c58aefe387df8f4325f15fce3
Jess <jessjdutton@gmail.com>
2020-11-21 19:07:58 -0800

    implemented search functionality

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def customers():
     print(f"All Customers in DB: {result}")
     return render_template('customers.html', rows=result)
 
-@app.route('/engineer_pods.html')
+@app.route('/engineer_pods.html', methods=['POST', 'GET'])
 def engineer_pods():
     if request.method == 'GET':
         db_connection = connect_to_database()
@@ -86,29 +86,38 @@ def engineer_pods():
 @app.route('/engineers.html', methods=['POST', 'GET'])
 def engineers():
     if request.method == 'GET':
-        db_connection = connect_to_database()
+        db_connection = connect_to_database()     
         query = "SELECT engineerID, firstName, lastName, available FROM Service_Engineers;"
         result = execute_query(db_connection, query).fetchall()
         print(f"All Engineers in DB: {result}")
         return render_template('engineers.html', rows=result)
 
-    if request.method == 'POST':
+    if request.method == 'POST' and 'firstName' in request.form:
         firstName = request.form['firstName']
         lastName = request.form['lastName']
         available = request.form['available']
-
         db_connection = connect_to_database()
         query = "INSERT INTO Service_Engineers (firstName, lastName, available) VALUES (%s, %s, %s)"
         data = firstName, lastName, available
         execute_query(db_connection, query, data)
         return redirect(url_for('engineers'))
+  
+    if request.method == 'POST':
+        lastName = request.form['lastName'] 
+        db_connection = connect_to_database()   
+        query = "SELECT engineerID, firstName, lastName, available FROM Service_Engineers WHERE lastName = '%s'" %(lastName)
+        searchresult = execute_query(db_connection, query).fetchall()
+        print(f"Search Result: {searchresult}")
+        query = "SELECT engineerID, firstName, lastName, available FROM Service_Engineers;"
+        result = execute_query(db_connection, query).fetchall()
+        print(f"All Engineers in DB: {result}")
+        return render_template('engineers.html', searchrows=searchresult, rows= result)
        
     else:
         return render_template('engineers.html')
 
+
         
-
-
 @app.route('/locations.html', methods=['GET', 'POST'])
 def locations():
     if request.method == 'GET':
@@ -139,7 +148,7 @@ def pods():
         print(f"All Pods in DB: {result}")
         return render_template('pods.html', rows=result)
 
-    if request.method == 'POST':
+    if request.method == 'POST' and 'operableStatus' in request.form:
         operableStatus = request.form['operableStatus']
         seatCapacity = '5'
         availableSeat = '5'
@@ -151,6 +160,18 @@ def pods():
         data = operableStatus, seatCapacity, availableSeat, inTransition, currentLocation
         execute_query(db_connection, query, data)
         return redirect(url_for('pods'))
+
+    if request.method == 'POST':
+        podID = request.form['podID'] 
+        db_connection = connect_to_database()   
+        query = "SELECT podID, operableStatus, seatCapacity, availableSeat, inTransition, currentLocation FROM Transport_Pods WHERE podID = '%s'" %(podID)
+        searchresult = execute_query(db_connection, query).fetchall()
+        print(f"Search Result: {searchresult}")
+        query = "SELECT podID, operableStatus, seatCapacity, availableSeat, inTransition, currentLocation FROM Transport_Pods;"
+        result = execute_query(db_connection, query).fetchall()
+        print(f"All Pods in DB: {result}")
+        return render_template('pods.html', searchrows=searchresult, rows= result)
+       
        
     else:
         return render_template('pods.html')
